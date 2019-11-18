a618a70f18b03e3a6c083e6606b2eae19d154ec3
Francesc Ferre <francesc.ferre@estudiants.urv.cat>
2019-11-18 19:41:56 +0100

    master: added main.cpp

---
+++ src/main.cpp
@@ -0,0 +1,21 @@
+#include <mbed.h>
+//#include <AFMotor.h>
+
+// Definicio de les constants
+#define FULL_SPEED 255
+#define STOPPED 0
+#define W_DIAMETER 6.6 //cm
+#define L_RODA (M_PI * W_DIAMETER) //2O.7345 cm
+
+
+int main() {
+
+  // put your setup code here, to run once:
+
+  
+
+  while(1) {
+    // put your main code here, to run repeatedly:
+    
+  }
+}
