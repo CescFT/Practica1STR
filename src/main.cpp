8fcac0717ee45ee9d2ec5516515313a832cf4ee2
Francesc Ferre <francesc.ferre@estudiants.urv.cat>
2019-11-21 18:52:44 +0100

    master: button added.

--- src/main.cpp
+++ src/main.cpp
@@ -7,10 +7,31 @@
 #define W_DIAMETER 6.6 //cm
 #define L_RODA (M_PI * W_DIAMETER) //2O.7345 cm
 
+AF_DCMotor m(1, MOTOR12_64KHZ);
+double motorSpeed=0.0; //Setpoint
+double Input=0.0;
+double Output=0.0;
+//2 5
+double Kp = 2;
+double Ki = 5;
+double Kd = 0;
+PID pid(&Input, &Output, &motorSpeed, Kp, Ki, Kd, DIRECT);
+
+volatile int num_sensor=0;
+
+int teclat = 0;
+int contador = 0;
+int control_contador=0;
+int TotalTime;
+int iTime;
+int fTime;
+
+int sensor = 18;
 
 int main() {
 
   // put your setup code here, to run once:
+
 
   
 
