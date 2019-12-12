14c578a8b7ee75ee490447d54178db2bee64e7ef
Francesc Ferre <francesc.ferre@estudiants.urv.cat>
2019-12-12 19:00:21 +0100

    master: modificacions.

--- src/main.cpp
+++ src/main.cpp
@@ -2,100 +2,92 @@
 #include "DCMotor.h"
 #include "PID.h"
 #include <math.h>
+#include <stdio.h>
 
 // Definicio de les constants
-#define FULL_SPEED 255
-#define STOPPED 0
-#define W_DIAMETER 6.6 //cm
-#define L_RODA (M_PI * W_DIAMETER) //2O.7345 cm
+#define MAX_SPEED 255
+#define MIN_SPEED 0
+#define DIRECT 0
+#define DIAMETER 6.6 //cm
+#define PERIMETER 20.7345 //2O.7345 cm
+#define PIN_SENSOR PG_0
 
-Motor m(1, MOTOR12_64KHZ);
-double motorSpeed=0.0; //Setpoint
-double Input=0.0;
-double Output=0.0;
+Motor m(0);
+double setPoint=0.0;
+double input=0.0;
+double output=0.0;
 //2 5
-double Kp = 2;
-double Ki = 5;
+double kp = 2;
+double ki = 5;
 double Kd = 0;
-PID pid(&Input, &Output, &motorSpeed, Kp, Ki, Kd, DIRECT);
+PID pid(&input, &output, &setPoint, kp, ki, Kd, 0 ,DIRECT);
 
-volatile int num_sensor=0;
-
+int steps=0;
+char reached=0;
 int teclat = 0;
-int contador = 0;
-int control_contador=0;
-int TotalTime;
+int printControl=0;
+int timeElapsed;
 
-
-int sensor = 18;
 Timer t;
-InterruptIn interrupt;
+InterruptIn interrupt((PinName)PIN_SENSOR);
 Serial serial1(USBTX, USBRX);
 
 
-void count(){
-  num_sensor++;
+int askKeyboardSpeed()
+{
+  serial1.printf("Please, set a speed for the motor (0-100):");
+  int keyboardSpeed;
+  scanf("%d", &keyboardSpeed);
+  keyboardSpeed = (int) sqrt(keyboardSpeed*keyboardSpeed)%100;
+
+  if(keyboardSpeed == 0) m.Direction(M_RELEASE);
+  return keyboardSpeed;
 }
 
+void count(){
+  steps++;
+}
 
 int main() {
+
+  char outputStr[100];
+  char inputStr[100];
+  char velMotorStr[100];
 
   // put your setup code here, to run once:
   interrupt.enable_irq();
   interrupt.rise(&count);
-  t.start();
   pid.SetMode(AUTOMATIC);
-  pid.SetOutputLimits(STOPPED, FULL_SPEED);
-  m.run(M_FORWARD);
-  m.setSpeed(Output);
-  char output[100];
-  char input[100];
-  char velMotor[100];
-  char numsensor[100];
+  m.Direction(M_FORWARD);
+  m.setSpeed(100.0, 100.0, 100.0, 100.0);
 
+  setPoint= 100;//askKeyboardSpeed();
+  t.start();
   while(1) {
     // put your main code here, to run repeatedly:
-    serial1.printf("Introduex la velocitat a la que vols que arribi el motor:");
-    char  vel = serial1.getc();
-    const char * str;
-    sscanf(str, "%d", &vel);
-    int velocitat = atoi(str);
-    while(velocitat > FULL_SPEED || velocitat <= STOPPED){
-      serial1.printf("Velocitat fora de rangs o es zero, introduex una velocitat correcte.");
-      vel = serial1.getc();
-      sscanf(str, "%d", &vel);
-      velocitat = atoi(str);
-      if(velocitat == 0)
-        m.run(M_RELEASE);
+    if(reached==1){
+        reached=0;
+        setPoint=askKeyboardSpeed();
     }
-    
-    motorSpeed=velocitat;
-    contador++;
 
-    if(num_sensor >= 80){
+    if(steps >= 80){//Cada dos voltes perque el sensor no esta ben calibrat
+                    //una volta real son dos del sensor
       t.stop();
-      TotalTime = t.read();
-      Input = (L_RODA /TotalTime) *1000; //cm/s la velocitat
-      num_sensor=0;
-      control_contador++;
-      if(control_contador == 8){
-        serial1.printf("PID: ");
-        sprintf(output, "%lf \n", Output);
-        serial1.printf(output);
-        serial1.printf("-----------------");
-        sprintf(input, "%lf\n", Input);
-        serial1.printf(input);
-        serial1.printf("cm/s");
-        sprintf(velMotor, "%lf\n", motorSpeed);
-        serial1.printf(velMotor);
-        serial1.printf("cm/s");
-        serial1.printf("Num sensor:");
-        sprinf(numsensor, "%d\n", num_sensor);
-        serial1.printf(numsensor);
-        control_contador=0;
+      timeElapsed = t.read();
+      input = (PERIMETER*(steps/80)/timeElapsed) *1000; //cm/s la speed
+      steps=0;
+      printControl++;
+      if(printControl == 5){
+        sprintf(outputStr, "PID: %lf \n", output);
+        serial1.printf(outputStr);
+        sprintf(inputStr, "Input: %lf cm/s\n", input);
+        serial1.printf(inputStr);
+        sprintf(velMotorStr, "Vel: %lf cm/s\n", setPoint);
+        serial1.printf(velMotorStr);
+        printControl=0;
       }
     }
     pid.Compute();
-    m.setSpeed(Output);
+    
   }
 }
